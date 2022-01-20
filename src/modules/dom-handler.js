6c506b012cbf7316b98c96aded49ff52ac1c12c6
Hopac1 <chris1101@hotmail.co.uk>
2022-01-20 14:37:56 +0000

    implement createTodo function

--- src/modules/dom-handler.js
+++ src/modules/dom-handler.js
@@ -1,3 +1,5 @@
+const todoContainer = document.querySelector(".todos-container");
+
 function createElement(elementName, className) {
     const element = document.createElement(elementName);
     element.classList = className;
@@ -12,13 +14,33 @@ function appendElement(parentElement, childElement) {
     return parentElement.appendChild(childElement);
 };
 
-function createTodo(todoName) {
+function createTodo(todoText) {
     const todo = createElement("div", "todo");
-    const todoPanelLeft = createElement("div", "task-panel-left");
-    // create:
-    // <i class="far fa-circle"></i>
-    // <p>First to-do</p>
-    // and append elements to their respective parents
+    const todoPanelLeft = createElement("div", "todo-panel-left");
+    const todoPanelRight = createElement("div", "todo-panel-right");
+
+    // circle image
+    const circle = createElement("i", "far fa-circle");
+    todoPanelLeft.appendChild(circle);
+    
+    // to-do text
+    const pTag = createElement("p", "todo-text");
+    pTag.textContent = todoText;
+    todoPanelLeft.appendChild(pTag);
+
+    // time to-do was submitted
+    const timeSubmitted = createElement("p", "time-submitted");
+    timeSubmitted.textContent = "5 mins ago";  // Generate actual date/time to-do was submitted
+    todoPanelRight.appendChild(timeSubmitted);
+
+    // bin image
+    const bin = createElement("i", "fas fa-trash-alt");
+    todoPanelRight.appendChild(bin);
+    
+
+    todo.appendChild(todoPanelLeft);
+    todo.appendChild(todoPanelRight);
+    todoContainer.appendChild(todo);
 };
 
 
