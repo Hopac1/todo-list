bf8cd4b83b76e83222c6e1aafac2b5de8a9c3afc
Hopac1 <chris1101@hotmail.co.uk>
2022-01-21 12:31:48 +0000

    implement logic to open and close the add project modal

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { createTodo } from "./modules/dom-handler";
+import { createTodo, setElementToActive } from "./modules/dom-handler";
 
 console.log("henlo world");
 
@@ -7,4 +7,18 @@ createTodo("do laundry");
 createTodo("watch tv");
 
 createTodo("watch tv");
+const addProjectBtn = document.querySelector(".add-project");
+const addProjectPopup = document.querySelector(".add-project-popup");
 
+const cancelProjectPopup = document.querySelector(".cancel-project");
+
+addProjectBtn.addEventListener("click", () => {
+    setElementToActive(addProjectBtn);
+    setElementToActive(addProjectPopup);
+
+});
+
+cancelProjectPopup.addEventListener("click", () => {
+    setElementToActive(addProjectPopup);
+    setElementToActive(addProjectBtn);
+})
